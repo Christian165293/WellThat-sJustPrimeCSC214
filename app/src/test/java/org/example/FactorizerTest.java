3b32c9dfc96499d2631bcbb30bb55363f8f46367
Christian165293 <christian.bourquin05@gmail.com>
2025-01-27 15:09:22 -0600

    Tests have been added and the code runs great.Its ready to be submitted!

--- app/src/test/java/org/example/FactorizerTest.java
+++ app/src/test/java/org/example/FactorizerTest.java
@@ -3,11 +3,36 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 class FactorizerTest {
   @Test
   void itReturnsAnEmptyListForZero() {
     Factorizer factorizer = new Factorizer();
-    assertEquals(factorizer.primeFactors(0), new ArrayList());
+    assertEquals(new ArrayList<>(), factorizer.primeFactors(0));
+  }
+  @Test
+  void itReturnsCorrectFactorsForPrimeNumber() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(List.of(13), factorizer.primeFactors(13));
+  }
+
+  @Test
+  void itReturnsCorrectFactorsForNonPrimeNumber() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(Arrays.asList(2, 2, 3), factorizer.primeFactors(12));
+  }
+
+  @Test
+  void itReturnsCorrectFactorsForAnotherNonPrimeNumber() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(Arrays.asList(3, 3, 3, 3), factorizer.primeFactors(81));
+  }
+
+  @Test
+  void itReturnsCorrectFactorsForLargeNumber() {
+    Factorizer factorizer = new Factorizer();
+    assertEquals(Arrays.asList(5, 7, 7, 1327), factorizer.primeFactors(325115));
   }
 }
