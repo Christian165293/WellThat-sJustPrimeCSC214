94c1d7618c9b873601b8c04bb20b4223ffd09715
Christian165293 <christian.bourquin05@gmail.com>
2025-01-27 14:52:10 -0600

    Code is running as it should, both the driver and the Factorizer.java class are working.Now I need to create test for this Lab and it will be done

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
-  public static void main(String[] args) {
-    // Test your Factorizer class using a driver program here!
-  }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Factorizer factorizer = new Factorizer();
+        int chosenInteger;
+        while (true) {
+            System.out.println("please enter a integer you would like to find the prime numbers for, or enter -100 to end the program");
+            chosenInteger = scanner.nextInt();
+            if (chosenInteger == -100) {
+                break;
+            } else
+                System.out.println("the prime factors of " + chosenInteger + " are" + factorizer.primeFactors(chosenInteger));
+        }
+    }
 }
--- app/src/main/java/org/example/Factorizer.java
+++ app/src/main/java/org/example/Factorizer.java
@@ -3,8 +3,24 @@ package org.example;
 import java.util.ArrayList;
 
 public class Factorizer {
-  public ArrayList<Integer> primeFactors(Integer n) {
-    // Implement your code here!
-    return new ArrayList<>();
-  }
+    public ArrayList<Integer> primeFactors(Integer n) {
+        ArrayList<Integer> primeNumbers = new ArrayList<>();
+        if (n == 0) {
+            return primeNumbers;
+        }
+        while (n % 2 == 0) {
+            primeNumbers.add(2);
+            n /= 2;
+        }
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            while (n % i == 0) {
+                primeNumbers.add(i);
+                n /= i;
+            }
+        }
+        if (n > 2) {
+            primeNumbers.add(n);
+        }
+        return primeNumbers;
+    }
 }
